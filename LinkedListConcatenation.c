27189a3d3e5da50c05f0bd05cd80037f262bebac
AparnaIyer06 <111898420+AparnaIyer06@users.noreply.github.com>
2025-01-29 12:58:53 +0530

    Add files via upload

---
+++ LinkedListConcatenation.c
@@ -0,0 +1,19 @@
+//Concatenate 2 Linked Lists
+#include <stdio.h>
+#include <conio.h>
+#include <malloc.h>
+
+struct node{
+	int data;
+	struct node*next;
+}head=NULL;
+
+
+void insert(s)
+
+int main(){
+	
+	
+	
+	
+}
